6c5e12d28ad6e386a20f51982f32baf6f68078c0
arun kumar <arunkumarpostbox2369@gmail.com>
2022-10-29 20:58:50 +0530

    first commit

---
+++ src/components/ExpensiveItem.js
@@ -0,0 +1,16 @@
+function ExpensiveItem(){
+    return(
+        <div>
+            <div>Food Rs10</div>
+            <div>
+                <div>Petrol Rs100</div>
+                <div>Movie Rs200</div>
+            </div>
+        </div>
+
+    );
+   
+    
+}
+
+export default ExpensiveItem;
