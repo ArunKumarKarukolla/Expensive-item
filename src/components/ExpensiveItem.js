e555822e6a2ac91e73f26f906e16f87a7bbb0dcf
arun kumar <arunkumarpostbox2369@gmail.com>
2022-11-01 23:01:53 +0530

    first commit

--- src/components/ExpensiveItem.js
+++ src/components/ExpensiveItem.js
@@ -1,10 +1,21 @@
-function ExpensiveItem(){
+import './ExpensiveItems.css';
+
+
+function ExpensiveItem(props){
+    const expensiveDate=new Date(2022,11,2);
+    const expensiveTitle='Car Insure';
+    const expensiveAmount='500.52';
+    const LocationOfExpenditure='Hyderabad';
+
     return(
         <div>
-            <div>Food Rs10</div>
-            <div>
-                <div>Petrol Rs100</div>
-                <div>Movie Rs200</div>
+            <div className='expensive-item'></div>
+            <div>{LocationOfExpenditure}-</div>
+            <div>{expensiveDate.toISOString()}</div>
+            <div className='expensive-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expensive-item__price'>{expensiveAmount}</div>
+
             </div>
         </div>
 
