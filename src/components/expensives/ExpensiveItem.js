b614ea4c5461c79c2eda7bd66d8621bd34798c93
arun kumar <arunkumarpostbox2369@gmail.com>
2022-11-15 18:17:36 +0530

    first commit

--- src/components/expensives/ExpensiveItem.js
+++ src/components/expensives/ExpensiveItem.js
@@ -1,21 +1,28 @@
-//import './ExpensiveItems.css';
+import React,{useState} from 'react';
 import ExpensiveDate from './ExpensiveDate';
 import Card from '../UI/Card';
 import './ExpensiveItems.css';
 
 
 function ExpensiveItem(props){
-    const expensiveDate=new Date(2022,11,2);
-    const expensiveTitle='Car Insure';
-    const expensiveAmount='500.52';
+    //const expensiveDate=new Date(2022,11,2);
+    //const expensiveTitle='Car Insure';
+    //const expensiveAmount='500.52';
     const LocationOfExpenditure='Hyderabad';
 
-    const month=props.date.getMonth();
-    const day=props.date.getDate();
-    const year=props.date.getFullYear();
+    const [title,setTitle]=useState(props.title);
+    const [amount,setExpensiveAmount]=useState(props.amount);
+
 
     const clickHandler=()=>{
-        console.log('clicked!!');
+        setTitle('Washdish','Laptop','LifeInsure','Table');
+        console.log(title);
+    }
+
+    const clickExpensive=()=>{
+        setExpensiveAmount('100$')
+        console.log(amount);
+
     }
 
     return(
@@ -23,11 +30,12 @@ function ExpensiveItem(props){
             <ExpensiveDate date={props.date}/>
             <div>{LocationOfExpenditure}-</div>
             <div className='expensive-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expensive-item__price'>{expensiveAmount}</div>
+                <h2>{title}</h2>
+                <div className='expensive-item__price'>{amount}</div>
 
             </div>
             <button onClick ={clickHandler}>Delete Expensive</button>
+            <button onClick ={clickExpensive}>Each expensive</button>
         </Card>
 
     );
