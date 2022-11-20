f4a7bf0dc3644b796ea7571154e722b525ce0873
arun kumar <arunkumarpostbox2369@gmail.com>
2022-11-20 20:16:57 +0530

    first commit

--- src/components/NewExpensive/ExpensiveForm.js
+++ src/components/NewExpensive/ExpensiveForm.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React,{useState} from "react";
 import './ExpensiveForm.css';
 
 const ExpensiveForm =()=>{
+    const [enterTitle,setEnterTitle]=useState('');
+    const [enterAmount,setEnterAmount]=useState('');
+    const [enterDate,setEnterDate]=useState('');
+
+
     const titleChangeHandler=(event)=>{
-        console.log(event.target.value)
-    }
-    return <form>
+        setEnterTitle(event.target.value)
+    };
+
+    const amountChangeHandler=(event)=>{
+        setEnterAmount(event.target.value);
+
+    };
+
+    const dateChangeHandler=(event)=>{
+        setEnterDate(event.target.value);
+
+    };
+
+    const submittHandler=(event)=>{
+        event.preventDefault();
+
+        const expensiveData={
+            title:enterTitle,
+            amount:enterAmount,
+            date:new Date(enterDate)
+        }
+        console.log(expensiveData);
+
+    };
+
+
+    return <form onSubmit={submittHandler}>
         <div className="new-expense__controls">
             <div className="new=expense__controls">
                 <label>Title</label>
@@ -13,11 +42,11 @@ const ExpensiveForm =()=>{
             </div>
             <div className="new=expense__controls">
                 <label>Amount</label>
-                <input type="number"  min="0.01" step="0.01"/>
+                <input type="number"  min="0.01" step="0.01" onChange={amountChangeHandler}/>
             </div>
             <div className="new=expense__controls">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"  />
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
             </div>
 
                 
